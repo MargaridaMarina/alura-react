d80a933addcc92e8b5ab683e8b9c9993b8f86038
Margarida <margaridammcm@gmail.com>
2022-05-24 08:52:50 -0300

    Estilizando botão

--- src/components/botao/index.tsx
+++ src/components/botao/index.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 class Botao extends React.Component {
   render (){
+    const isActive = false;
+    const styles = {
+      backgroundColor: isActive ? "green" : "red"
+    }
     return (
-      <button>
+      <button style={styles}>
         Botão
       </button>
     )
