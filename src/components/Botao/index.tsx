d758d595a88d4f5cf59886a7f500645499ea0385
Margarida <margaridammcm@gmail.com>
2022-06-04 18:49:56 -0300

    Adicionando tipo do botao

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import style from './Botao.module.scss'
 
-class Botao extends React.Component <any, any>{
+class Botao extends React.Component <any,{
+  type?: "button" | "submit" | "reset" | undefined
+}>{
   render (){
-    return (
-      <button className={style.botao}>
+    const {type = "button"} = this.props;
+     return (
+      <button type={type} className={style.botao}>
         {this.props.children}
       </button>
     )
